cae316e3de72034927e27b1a333df10247f31a71
Unknown <xunpeng715@gmail.com>
2018-04-18 12:05:31 -0400

    update model

--- model.py
+++ model.py
@@ -105,3 +105,252 @@ class MemN2N(object):
             a = tf.add(o2dim, self.hid[-1]) # [batch_size, edim]
             self.hid.append(a)              # [input, a_1, a_2, ..., a_nhop]
     
+    def build_model(self):
+        self.build_memory()
+        
+        self.W = tf.Variable(tf.random_normal([self.edim, self.nwords], mean=self.init_mean, stddev=self.init_std))
+        a_hat = tf.matmul(self.hid[-1], self.W)
+        
+        self.hypothesis = tf.nn.softmax(a_hat)
+
+        self.loss = tf.nn.softmax_cross_entropy_with_logits(logits=a_hat, labels=self.target)
+        
+        self.lr = tf.Variable(self.current_lr)
+        self.opt = tf.train.GradientDescentOptimizer(self.lr)
+        
+        params = [self.A_, self.B_, self.C_, self.T_A_, self.T_C_, self.W]
+        grads_and_vars = self.opt.compute_gradients(self.loss, params)
+        clipped_grads_and_vars = [(tf.clip_by_norm(gv[0], self.max_grad_norm), gv[1]) for gv in grads_and_vars]
+        
+        inc = self.global_step.assign_add(1)
+        with tf.control_dependencies([inc]):
+            self.optim = self.opt.apply_gradients(clipped_grads_and_vars)
+        
+        tf.global_variables_initializer().run()
+        self.saver = tf.train.Saver()
+
+
+    def train(self, train_stories, train_questions):
+        N = int(math.ceil(len(train_questions) / self.batch_size))
+        cost = 0
+        
+        if self.show_progress:
+            bar = ProgressBar('Train', max=N)
+        
+        for idx in range(N):
+            
+            if self.show_progress:
+                bar.next()
+            
+            if idx == N - 1:
+                iterations = len(train_questions) - (N - 1) * self.batch_size
+            else:
+                iterations = self.batch_size
+            
+            query = np.ndarray([iterations, self.max_words], dtype=np.int32)
+            time = np.zeros([iterations, self.mem_size], dtype=np.int32)
+            target = np.zeros([iterations, self.nwords], dtype=np.float32)
+            context = np.ndarray([iterations, self.mem_size, self.max_words], dtype=np.int32)
+            
+            for b in range(iterations):
+                m = idx * self.batch_size + b
+                
+                curr_q = train_questions[m]
+                q_text = curr_q['question']
+                story_ind = curr_q['story_index']
+                sent_ind = curr_q['sentence_index']
+                answer = curr_q['answer'][0]
+                
+                curr_s = train_stories[story_ind]
+                curr_c = curr_s[:sent_ind + 1]
+
+                if len(curr_c) >= self.mem_size:
+                    curr_c = curr_c[-self.mem_size:]
+                    
+                    for t in range(self.mem_size):
+                        time[b, t].fill(t)
+                else:
+                    
+                    for t in range(len(curr_c)):
+                        time[b, t].fill(t)
+                    
+                    while len(curr_c) < self.mem_size:
+                        curr_c.append([0.] * self.max_words)
+                
+                query[b, :] = q_text
+                target[b, answer] = 1
+                context[b, :, :] = curr_c
+
+            _, loss, self.step = self.sess.run([self.optim, self.loss, self.global_step],
+                                               feed_dict={self.query: query, self.time: time,
+                                                          self.target: target, self.context: context})
+            cost += np.sum(loss)
+        
+        if self.show_progress:
+            bar.finish()
+        
+        return cost / len(train_questions)
+    
+    
+    def test(self, test_stories, test_questions, label='Test'):
+        N = int(math.ceil(len(test_questions) / self.batch_size))
+        cost = 0
+        
+        if self.show_progress:
+            bar = ProgressBar('Train', max=N)
+        
+        for idx in range(N):
+            
+            if self.show_progress:
+                bar.next()
+            
+            if idx == N - 1:
+                iterations = len(test_questions) - (N - 1) * self.batch_size
+            else:
+                iterations = self.batch_size
+            
+            query = np.ndarray([iterations, self.max_words], dtype=np.int32)
+            time = np.zeros([iterations, self.mem_size], dtype=np.int32)
+            target = np.zeros([iterations, self.nwords], dtype=np.float32)
+            context = np.ndarray([iterations, self.mem_size, self.max_words], dtype=np.int32)
+            
+            for b in range(iterations):
+                m = idx * self.batch_size + b
+                
+                curr_q = test_questions[m]
+                q_text = curr_q['question']
+                story_ind = curr_q['story_index']
+                sent_ind = curr_q['sentence_index']
+                answer = curr_q['answer'][0]
+                
+                curr_s = test_stories[story_ind]
+                curr_c = curr_s[:sent_ind + 1]
+                
+                if len(curr_c) >= self.mem_size:
+                    curr_c = curr_c[-self.mem_size:]
+                    
+                    for t in range(self.mem_size):
+                        time[b, t].fill(t)
+                else:
+                    
+                    for t in range(len(curr_c)):
+                        time[b, t].fill(t)
+                    
+                    while len(curr_c) < self.mem_size:
+                        curr_c.append([0.] * self.max_words)
+                
+                query[b, :] = q_text
+                target[b, answer] = 1
+                context[b, :, :] = curr_c
+
+            _, loss, self.step = self.sess.run([self.optim, self.loss, self.global_step],
+                                               feed_dict={self.query: query, self.time: time,
+                                                          self.target: target, self.context: context})
+            cost += np.sum(loss)
+        
+        if self.show_progress:
+            bar.finish()
+        
+        return cost / len(test_questions)
+    
+    
+    def run(self, train_stories, train_questions, test_stories, test_questions):
+        if not self.is_test:
+
+            for idx in range(self.nepoch):
+                train_loss = np.sum(self.train(train_stories, train_questions))
+                test_loss = np.sum(self.test(test_stories, test_questions, label='Validation'))
+                
+                self.log_loss.append([train_loss, test_loss])
+                
+                state = {
+                    'loss': train_loss,
+                    'epoch': idx,
+                    'learning_rate': self.current_lr,
+                    'validation_loss': test_loss
+                }
+                
+                print(state)
+                
+                
+                # learning rate annealing
+                if (not idx == 0) and (idx % self.anneal_epoch == 0):
+                    self.current_lr = self.current_lr * self.anneal_rate
+                    self.lr.assign(self.current_lr).eval()
+            
+                # If validation loss stops decreasing, insert softmax layers
+                if idx == 0:
+                    pass
+                else:
+                    if self.log_loss[idx][1] > self.log_loss[idx - 1][1]:
+                        self.lin_start = False
+
+                if idx % 10 == 0:
+                    self.saver.save(self.sess,
+                                    os.path.join(self.checkpoint_dir, "MemN2N.model"),
+                                    global_step=self.step.astype(int))
+        else:
+            self.load()
+            
+            valid_loss = np.sum(self.test(train_stories, train_questions, label='Validation'))
+            test_loss = np.sum(self.test(test_stories, test_questions, label='Test'))
+            
+            state = {
+                'validation_loss': valid_loss,
+                'test_loss': test_loss
+            }
+            
+            print(state)
+
+
+    def predict(self, test_stories, test_questions):
+        self.load()
+
+        num_instances = len(test_questions)
+
+        query = np.ndarray([num_instances, self.max_words], dtype=np.int32)
+        time = np.zeros([num_instances, self.mem_size], dtype=np.int32)
+        target = np.zeros([num_instances, self.nwords], dtype=np.float32)
+        context = np.ndarray([num_instances, self.mem_size, self.max_words], dtype=np.int32)
+
+        for b in range(num_instances):
+            
+            curr_q = test_questions[b]
+            q_text = curr_q['question']
+            story_ind = curr_q['story_index']
+            sent_ind = curr_q['sentence_index']
+            answer = curr_q['answer'][0]
+            
+            curr_s = test_stories[story_ind]
+            curr_c = curr_s[:sent_ind + 1]
+            
+            if len(curr_c) >= self.mem_size:
+                curr_c = curr_c[-self.mem_size:]
+                
+                for t in range(self.mem_size):
+                    time[b, t].fill(t)
+            else:
+                
+                for t in range(len(curr_c)):
+                    time[b, t].fill(t)
+                
+                while len(curr_c) < self.mem_size:
+                    curr_c.append([0.] * self.max_words)
+            
+            query[b, :] = q_text
+            target[b, answer] = 1
+            context[b, :, :] = curr_c
+
+        predictions = self.sess.run(self.hypothesis, feed_dict={self.query: query, self.time: time, self.context: context})
+
+        return predictions, target
+
+
+        
+    def load(self):
+        print(' [*] Reading checkpoints...')
+        ckpt = tf.train.get_checkpoint_state(self.checkpoint_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            self.saver.restore(self.sess, ckpt.model_checkpoint_path)
+        else:
+            raise Exception(" [!] No checkpoint found")
